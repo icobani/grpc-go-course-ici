9733399ae907ad63a35bb4025e53097bd52fdee7
ibrahimcobani <ibrahim@imaconsult.com>
2019-04-16 16:16:36 +0300

    calculator servisi

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
 protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
-##env env GOOS=linux GOARCH=amd64 go build
+##env GOOS=linux GOARCH=amd64 go build
